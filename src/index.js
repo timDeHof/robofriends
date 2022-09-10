7978479b2ecae4cb02cf0b0ea6b59eff69e647a7
timDeHof <this.is.tim.dehof@gmail.com>
2022-09-10 19:24:56 -0400

    created card and cardlist componts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { robots } from "./robots";
+import CardList from "./CardList";
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
